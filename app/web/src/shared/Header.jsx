ecf4bcb39533ae2bca055d646837a7fa3c6d6a99
Tayquan <adedejionigbinde@gmail.com>
2021-02-17 17:43:15 +0100

    commit9

--- app/web/src/shared/Header.jsx
+++ app/web/src/shared/Header.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Nav, Navbar,Form,FormControl, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 
 const Header = () => {
+    const [userCookie, setUserCookie] = useState('');
+
+    useEffect(() => {
+        const getCookie = () => {
+            if (document.cookie.includes('uid')) {
+                let uidRow = document.cookie.split(';').find(row => row.startsWith('uid'));
+                if (uidRow !== -1) {
+                     setUserCookie(uidRow.split('=')[1])
+                }
+            }
+            return false;
+        }
+        getCookie();
+        console.log(userCookie)
+    },[userCookie])
+    
     return (
 
         <Navbar bg='primary' variant='dark' className='justify-content-between' >
             <Nav>
                 <Navbar.Brand href='/'>Project Explorer</Navbar.Brand>
                 <Form inline>
-                    <FormControl type='text' placeholder='Search Project' className="mr-sm-2"/>
+                    <FormControl type='text' placeholder='Search Project' className="mr-sm-2" />
                     <Button variant='outline-light' type='submit'>Search</Button>
                 </Form>
                 <Nav>
