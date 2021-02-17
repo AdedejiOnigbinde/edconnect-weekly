7e4674319fabca631fe826b251ec2762b33afd53
Tayquan <adedejionigbinde@gmail.com>
2021-02-17 22:36:59 +0100

    commit12

--- app/web/src/shared/Header.jsx
+++ app/web/src/shared/Header.jsx
@@ -2,25 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 
+
 const Header = () => {
     const [userCookie, setUserCookie] = useState('');
     const [userId, setUserId] = useState('');
     const [user, setUser] = useState('');
     const history = useHistory();
 
-    useEffect(() => {
-        const getCookie = () => {
-            if (document.cookie.includes('uid')) {
-                let uidRow = document.cookie.split(';').find(row => row.startsWith('uid'));
-                if (uidRow !== -1) {
-                    setUserCookie(uidRow.split('=')[1])
-                }
+    const getCookie = () => {
+        if (document.cookie.includes('uid')) {
+            let uidRow = document.cookie.split(';').find(row => row.startsWith('uid'));
+            if (uidRow !== -1) {
+                setUserCookie(uidRow.split('=')[1])
             }
-            return false;
         }
+        return false;
+    }
 
+ 
+
+    useEffect(() => {
         const initUser = () => {
-            let url = window.location.href.split('/').pop();
             setUserId(userCookie)
             if (userId) {
                 fetch('http://localhost:4000/api/users/' + userId)
@@ -28,50 +30,46 @@ const Header = () => {
                         const resp = await response.json();
                         setUser(resp);
                     })
-            } else if (!user && url.startsWith('createProject.html')) {
-                history.push('/login')
             }
-
+            return false;
+    
         }
-        getCookie();
-        initUser();
-        return function cleanUp() {
-
-        }
-
-    })
+        getCookie()
+        initUser()
+    }, [userCookie,userId])
+    console.log(user)
 
     const logOut = () => {
         document.cookie = `uid= ; expires = Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
         history.push('/')
-};
+    };
 
-return (
+    return (
 
-    <Navbar bg='primary' variant='dark' className='justify-content-between' >
-        <Nav>
-            <Navbar.Brand href='/'>Project Explorer</Navbar.Brand>
-            <Form inline>
-                <FormControl type='text' placeholder='Search Project' className="mr-sm-2" />
-                <Button variant='outline-light' type='submit'>Search</Button>
-            </Form>
+        <Navbar bg='primary' variant='dark' className='justify-content-between' >
             <Nav>
-                <Nav.Link href="/projects">Project</Nav.Link>
-                <Nav.Link href="/projects/submit">Submit</Nav.Link>
+                <Navbar.Brand href='/'>Project Explorer</Navbar.Brand>
+                <Form inline>
+                    <FormControl type='text' placeholder='Search Project' className="mr-sm-2" />
+                    <Button variant='outline-light' type='submit'>Search</Button>
+                </Form>
+                <Nav>
+                    <Nav.Link href="/projects">Project</Nav.Link>
+                    <Nav.Link href="/projects/submit">Submit</Nav.Link>
+                </Nav>
             </Nav>
-        </Nav>
 
-        {!user && <Nav className='justify-content-end'>
-            <Nav.Link href="/signup">Sign Up</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-        </Nav>}
+            {!user && <Nav className='justify-content-end'>
+                <Nav.Link href="/signup">Sign Up</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>}
 
-        {user && <Nav className='justify-content-end'>
-            <Nav.Link onClick={logOut} >Log Out</Nav.Link>
-            <Navbar.Text>Hi + {user.firtname}</Navbar.Text>
-        </Nav>}
-    </Navbar>
-);
+            {user && <Nav className='justify-content-end'>
+                <Nav.Link onClick={logOut} >Log Out</Nav.Link>
+                <Navbar.Text><span>Hi {user.firtname}</span></Navbar.Text>
+            </Nav>}
+        </Navbar>
+    );
 }
 
 export default Header;
