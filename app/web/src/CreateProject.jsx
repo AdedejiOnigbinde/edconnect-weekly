20d62eb355e61c70fb074fec282e798ca386edde
Tayquan <adedejionigbinde@gmail.com>
2021-02-20 15:55:18 +0100

    Completion Of Week 7 Assignment

--- app/web/src/CreateProject.jsx
+++ app/web/src/CreateProject.jsx
@@ -7,39 +7,45 @@ import { getUserId } from './helper';
 const CreateProject = () => {
     const history = useHistory();
     const [errors, setErrors] = useState([])
-    const [state, setState] = useState({
-        projectName: "",
-        projectAbstract: "",
-    });
-
-    const [state2, setState2] = useState({
-        author: [],
-        tags: []
-    });
+    const [projectName, setprojectName] = useState("")
+    const [projectAbstract, setprojectAbstract] = useState("")
+    const [authors, setAuthors] = useState("")
+    const [tags, setTags] = useState("")
 
     const handleChange = e => {
-        setState({
-            ...state,
-            [e.target.name]: e.target.value,
-        });
+        const { name, value } = e.target;
+        switch (name) {
+            case 'projectName':
+                setprojectName(value);
+                break;
+            case 'projectAbstract':
+                setprojectAbstract(value);
+                break;
+            case 'authors':
+                setAuthors(value.split(","))
+                break;
+            case 'tags':
+                setTags(value.split(","))
+                break;
+            default:
+                return null;
+
+        }
     }
 
-    const handleChange2 = e => {
-        setState2({
-            ...state2,
-            [e.target.name]: e.target.value,
-        });
-    }
-    console.log()
     const postProject = () => {
-        
         fetch('/api/projects', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
 
-            body: JSON.stringify(state, state2)
+            body: JSON.stringify({
+                authors: authors,
+                tags: tags,
+                name: projectName,
+                abstract: projectAbstract
+            })
         }).then(async function (response) {
             const resp = await response.json()
             if (response.status === 200) {
@@ -62,19 +68,19 @@ const CreateProject = () => {
                     <h1>Submit Project</h1>
                     <Form.Group>
                         <Form.Label>Project name</Form.Label>
-                        <Form.Control type='text' placeholder="Enter project name" name='projectName' value={state.projectName} onChange={handleChange} />
+                        <Form.Control type='text' placeholder="Enter project name" name='projectName' value={projectName} onChange={handleChange} />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Project abstract</Form.Label >
-                        <Form.Control as="textarea" rows={8} name='projectAbstract' value={state.projectAbstract} onChange={handleChange} />
+                        <Form.Control as="textarea" rows={8} name='projectAbstract' value={projectAbstract} onChange={handleChange} />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Authors</Form.Label>
-                        <Form.Control type='text' placeholder="Enter authors name(seperated by commas)" name='author' value={state2.author} onChange={handleChange2} />
+                        <Form.Control type='text' placeholder="Enter authors name(seperated by commas)" name='authors' value={authors} onChange={handleChange} />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Tag(s)</Form.Label>
-                        <Form.Control type='text' placeholder="Use # to tag project different topics (e.g #javascript, #mongodb)" name='tags' value={state2.tags} onChange={handleChange2} />
+                        <Form.Control type='text' placeholder="Use # to tag project different topics (e.g #javascript, #mongodb)" name='tags' value={tags} onChange={handleChange} />
                     </Form.Group>
                     <Button variant='primary' onClick={postProject}>
                         Continue
