53faa57999965b00286bb6d3c2369e6248347f30
Tayquan <adedejionigbinde@gmail.com>
2021-02-28 22:42:48 +0100

    Completion Of Week 7 Assignment

--- app/web/src/helper.js
+++ app/web/src/helper.js
@@ -1,7 +1,7 @@
 export const getUserId = () => {
     if (document.cookie.includes('uid')) {
-        let uidRow = document.cookie.split(';').find(row => row.startsWith('uid'));
-        if (uidRow !== -1) {
+        let uidRow = document.cookie.split(';').find(row => row.trim().startsWith('uid'));
+        if (uidRow) {
             return uidRow.split('=')[1]
         }
     }
