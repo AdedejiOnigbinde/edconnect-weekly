68ac72bed2108ffa78ad920e2e17fea8e97f4b7f
Tayquan <adedejionigbinde@gmail.com>
2021-03-01 20:28:33 +0100

    final changes

--- app/web/src/Login.jsx
+++ app/web/src/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
 
     return (
         <Layout>
-            <Form className='w-50 mx-auto' controlId = 'loginForm'>
+            <Form className='w-50 mx-auto' id = 'loginForm'>
                 {error && <Alert key={'error' + error} variant={'danger'}>{error}</Alert>}
                 <h1>Login</h1>
                 <Form.Group>
