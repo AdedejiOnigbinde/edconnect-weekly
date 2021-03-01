fcc3a5eac25da742cf4cdbca12b200cab317f5f2
Tayquan <adedejionigbinde@gmail.com>
2021-03-01 20:40:33 +0100

    changes

--- app/web/src/helper.js
+++ app/web/src/helper.js
@@ -1,7 +1,7 @@
 export const getUserId = () => {
     if (document.cookie.includes('uid')) {
         let uidRow = document.cookie.split(';').find(row => row.trim().startsWith('uid'));
-        if (uidRow) {
+        if (uidRow !== -1) {
             return uidRow.split('=')[1]
         }
     }
